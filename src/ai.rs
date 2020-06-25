de5de23d041d745c6acb7a93276b61b75d923273
Benjamin Hough <benjaminhough@microvu.com>
2020-06-24 18:17:22 -0700

    Add alpha-beta pruning

--- src/ai.rs
+++ src/ai.rs
@@ -13,42 +13,37 @@ fn eval(b: &Board, d: i32) -> i32 {
 }
 
 pub fn get_move(b: &Board) -> Move {
-	get_move_r(&b, 9, O).1
+	get_move_r(&b, 9, O, -1000, 1000).1
 }
 
-fn get_move_r(b: &Board, d: i32, e: Entry) -> (i32, Move) {
+fn get_move_r(s: &Board, d: i32, e: Entry, mut a: i32, b: i32) -> (i32, Move) {
 	if d == 0 {
-		(e.to_int() * eval(b, d), 0)
+		(e.to_int() * eval(s, d), 0)
 	} else {
-		let mvs = b.generate_moves(e);
+		let mvs = s.generate_moves(e);
 
-		if mvs.1.len() == 0 || b.check_winner().is_some() {
-			(e.to_int() * eval(b, d), 0)
+		if mvs.1.len() == 0 || s.check_winner().is_some() {
+			(e.to_int() * eval(s, d), 0)
 		} else {
 			let mut value = -1000000;
 			let mut best_mv = 0;
-			let mut vs = [0; 9];
 
 			for &mv in &mvs.1 {
-				let b2 = b.apply_move(e, mv);
-				let next_val = -get_move_r(&b2, d-1, e.flip()).0;
-
-				if d == 9 {
-					vs[mv] = next_val;
-				}
+				let b2 = s.apply_move(e, mv);
+				let next_val = -get_move_r(&b2, d-1, e.flip(), -b, -a).0;
 
 				if next_val > value {
 					value = next_val;
 					best_mv = mv;
 				}
+
+				if next_val > a {
+					a = next_val;
+				}
+
+				if a >= b { break; }
 			}
 
-			if d == 9 { 
-				// println!("{:?}", mvs.1);
-				// println!("{:?}", &vs[0..3]);
-				// println!("{:?}", &vs[3..6]);
-				// println!("{:?}", &vs[6..9]);
-			}
 			(value, best_mv)
 		}
 	}
